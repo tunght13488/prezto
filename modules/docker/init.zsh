6311231ccf59800318e3bdb1c2b1eb4b535b7abe
Ha The Tung <tunght13488@gmail.com>
2015-02-27 11:48:01 +0700

    add docker alias

---
+++ modules/docker/init.zsh
@@ -0,0 +1,18 @@
+#
+# Provides Docker aliases
+#
+# Authors:
+#   Tung Ha <tunght13488@gmail.com>
+#
+
+# Return if requirements are not found.
+if (( ! $+commands[docker] )); then
+  return 1
+fi
+
+# Load dependencies.
+pmodload 'helper'
+
+# Source module files.
+source "${0:h}/alias.zsh"
+
