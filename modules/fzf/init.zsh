464ca1dd5729605e0956f575fbab48c5feb8f007
Ha The Tung <tunght13488@gmail.com>
2023-11-28 17:18:58 +0800

    update: ubuntu

--- modules/fzf/init.zsh
+++ modules/fzf/init.zsh
@@ -5,9 +5,15 @@ fi
 if [[ -r /usr/share/fzf/completion.zsh ]]; then
   source /usr/share/fzf/completion.zsh
 fi
+if [[ -r /usr/share/doc/fzf/examples/completion.zsh ]]; then
+  source /usr/share/doc/fzf/examples/completion.zsh
+fi
 
 if [[ -r /usr/share/fzf/key-bindings.zsh ]]; then
   source /usr/share/fzf/key-bindings.zsh
+fi
+if [[ -r /usr/share/doc/fzf/examples/key-bindings.zsh ]]; then
+  source /usr/share/doc/fzf/examples/key-bindings.zsh
 fi
 
 if [[ -r ${0:h}/functions.zsh ]]; then
